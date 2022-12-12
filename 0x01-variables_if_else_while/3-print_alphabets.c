bcb3f2341316f611bfe3e8edaa475968a28f761c
marthaakinlolu <marthaakinlolu@gmail.com>
2022-12-11 16:39:33 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints both lower and upper case.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
