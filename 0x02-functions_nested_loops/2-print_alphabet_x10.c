8514d6c66aebe94c5c13da69c603e406f5139862
marthaakinlolu <marthaakinlolu@gmail.com>
2022-12-13 12:35:34 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int ten;
+	char x;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (x = 'a'; x <= 'z'; x++
+			_putchar(x);
+		_putchar('\n');
+	}
+}
