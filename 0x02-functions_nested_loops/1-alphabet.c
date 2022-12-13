d40ff5bd408cf779002dd2108493779e3a618da0
marthaakinlolu <marthaakinlolu@gmail.com>
2022-12-13 11:41:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print the alphabets in lowercase
+ * next line
+ * prototype
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++
+			_putchar(x);
+
+	_putchar('\n');
+}
