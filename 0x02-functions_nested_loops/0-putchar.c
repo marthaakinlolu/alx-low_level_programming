281a4f39eeb1468139a1dccbcf0610a52be2852d
marthaakinlolu <marthaakinlolu@gmail.com>
2022-12-13 08:10:03 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints _putchar.
+ * followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+	char 
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,4 @@
+C - functions
+Nested while loops
+
+Write a program that prints _putchar
