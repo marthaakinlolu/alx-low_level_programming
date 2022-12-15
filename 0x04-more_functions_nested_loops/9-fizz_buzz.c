c48359586904ff02f2fc762594488bb0f935919c
marthaakinlolu <marthaakinlolu@gmail.com>
2022-12-15 08:08:37 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,28 @@ int main(void)
 {
 	int i;
 
-		for (i = 1; i <= 100; i++)
+	i = 1;
+	printf("%d", i);
+
+	for (i = 2; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			if (i % 15 == 0)
-				printf("FizzBuss");
-			else if (i % 3 == 0)
-				printf("Fizz");
-			else if (i % 5 == 0)
-				printf("Buss");
-			else
-				printf("%i", i);
-			if (i < 100)
-				printf(" ");
+			printf(" FizzBuzz");
 		}
+		else if (i % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
 	printf("\n");
 	return (0);
 }
