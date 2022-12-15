45f3d8da737be5c946f3301ede40bf959ad5c66f
marthaakinlolu <marthaakinlolu@gmail.com>
2022-12-15 08:53:33 -0800

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,29 +4,19 @@
  * print_triangle - prints a triangle
  * Description: prints diagonals
  * @size: size of the triangle
- * Return: 0
  */
 void print_triangle(int size)
 {
-	int row, hashes. spaces;
+	int i, j;
 
-	if (size <= 0)
+	for (i = 0; i < size; i++)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= sizes; row++)
-		{
-			for (spaces = size - row; spaces >= 1; spaces--)
-			{
-				_putchar(' ');
-			}
-			for (hashes = 1; hashes <= row; hashes++)
-			{
-				_putchar('#');
-			}
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar(35);
+		if (i < (size - 1))
 			_putchar('\n');
-		}
 	}
+	_putchar('\n');
 }
