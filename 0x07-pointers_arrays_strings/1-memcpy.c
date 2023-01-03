31b4d9763e610b7aa4bf30b82785285cb459403a
marthaakinlolu <marthaakinlolu@gmail.com>
2023-01-03 02:04:45 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,13 +14,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
 
 	return (dest);
 }
