8954132eaf64ae203b4947cf9facfa03c86c44f6
marthaakinlolu <marthaakinlolu@gmail.com>
2023-07-12 14:00:52 -0700

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -4,7 +4,6 @@
  * recursive_search - searches for a value in an array of
  * integers using the Binary search algorithm
  *
- *
  * @array: input array
  * @size: size of the array
  * @value: value to search in
