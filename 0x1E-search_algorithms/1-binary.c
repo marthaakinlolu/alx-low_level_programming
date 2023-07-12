80b7acdce0b54c099045e1425d27a350af62a431
marthaakinlolu <marthaakinlolu@gmail.com>
2023-07-12 14:49:18 -0700

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,12 @@
 #include "search_algos.h"
 
 /**
- * recursive_search - searches for a value in an array of
- * integers using the Binary search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
+ * binary_search - funct that searches for a value in a sorted array iterative
+ * of integers using the Binary search algorithm
+ * @array: Type pointer of given array
+ * @size: Type size of elements in the array
+ * @value: Type value to be searched
+ * Return: Value, or -1 if value not present
  */
 int binary_search(int *array, size_t size, int value)
 {
